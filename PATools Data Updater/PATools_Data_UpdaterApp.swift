ed52aa669e6268f5cdff256005bdc781d6d438f9
OJ <olivier.jobin@hotmail.fr>
2025-10-30 14:23:48 +0100

    Initial Commit

---
+++ PATools Data Updater/PATools_Data_UpdaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PATools_Data_UpdaterApp.swift
+//  PATools Data Updater
+//
+//  Created by Olivier Jobin on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PATools_Data_UpdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PATools Data UpdaterTests/PATools_Data_UpdaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PATools_Data_UpdaterTests.swift
+//  PATools Data UpdaterTests
+//
+//  Created by Olivier Jobin on 30/10/2025.
+//
+
+import Testing
+@testable import PATools_Data_Updater
+
+struct PATools_Data_UpdaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
